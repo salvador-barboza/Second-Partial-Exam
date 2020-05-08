63c1d405b662851bf68a78aeda70337e2c00be9a
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2020-05-08 09:17:58 -0500

    Remove extra comments from ex1 and add some missing ui

--- Coding-Challenge-1/js/index.js
+++ Coding-Challenge-1/js/index.js
@@ -1,19 +1,3 @@
-// Open the folder “Coding-Challenge-1”.
-// b. This is a small app that shows the recipes of meals. You will find an application that has an
-// index.html page already set up with a form, however, the JavaScript functionality in the index.js file
-// is missing.
-// c. Add the code that is needed so that when the user clicks the button “Get meals” and provides the
-// name of a meal in the given input, you will connect to theMealDB API and from the list of results you
-// will display in screen the following (This goes for every single meal in the list):
-// i. Complete meal name
-// ii. Meal area/cuisine
-// iii. Meal’s instructions of preparation.
-// iv. Meal’s picture
-// d. The documentation of this API is located at: https://www.themealdb.com/api.php
-// e. Use the appropriate endpoint to retrieve the recipes of the meals matching the search criteria.
-// f. In case of receiving an error or zero results from the API show in screen a message with the
-// following: “Meal not found”.
-
 function getMealByName(name) {
   const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
 
@@ -33,8 +17,9 @@ function mealItem({ mealName, cuisine, instructions, picture }) {
   const el = document.createElement('li')
   el.innerHTML = `
     <h2>${mealName}</h2>
-    <h4>${cuisine}</h4>
-    <img src="${picture}" />
+    <h4>Cuisine: ${cuisine}</h4>
+    <img height=200 src="${picture}" />
+    <h4>Instructions</h4>
     <p>${instructions}</p>
   `
 
